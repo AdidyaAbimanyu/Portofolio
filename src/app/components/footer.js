95ad789f0fa49e89bf893c2b525fb932828db8b5
Adidya Abimanyu <114373205+AdidyaAbimanyu@users.noreply.github.com>
2025-01-13 01:46:07 +0700

    First Commit

---
+++ src/app/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="text-center">
+                <p>&copy; 2024 Adidya Abimanyu</p>
+            </div>
+        </footer>
+    );
+}
